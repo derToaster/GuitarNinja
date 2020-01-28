b2b2e92afdc7cd36cd35ae2e447f4a40955a6d33
ashkanmoradi <asorrow1214@gmail.com>
2020-01-28 16:57:09 +0100

    _Meta

---
+++ Assets/Scripts/GameOverScreen/Highscore.cs
@@ -0,0 +1,53 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Highscore : MonoBehaviour {
+    private int sessionScores, highScores;
+
+
+    public TextMeshProUGUI  highscore, congratulations, newHighscore;
+
+    private Transform gameOver;
+
+    private GameObject highScorePanel;
+    
+
+    // Start is called before the first frame update
+    void Start() {
+
+        gameOver = GameObject.Find("GameOver").transform;
+        sessionScores = PlayerPrefs.GetInt(Tags.SESSIONSCORES);
+        
+        if (!PlayerPrefs.HasKey(Tags.HIGHSCORES)) {// if Highscore does not exist
+            PlayerPrefs.SetInt(Tags.HIGHSCORES, 0);
+        }
+        else {
+            highScores = PlayerPrefs.GetInt(Tags.HIGHSCORES);
+
+
+
+
+        }
+
+
+
+
+
+        if (sessionScores > highScores) {
+            PlayerPrefs.SetInt(Tags.HIGHSCORES, sessionScores);
+            gameOver.position = new Vector3(gameOver.position.x, gameOver.position.y - 90, gameOver.position.z);
+            congratulations.enabled = true;
+            newHighscore.enabled = true;
+        }
+        else {
+            PlayerPrefs.DeleteKey(Tags.SESSIONSCORES);
+            gameOver.position = new Vector3(gameOver.position.x, gameOver.position.y , gameOver.position.z);
+            congratulations.enabled = false;
+            newHighscore.enabled = false;
+
+        }
+
+
+        highscore.SetText("Highscore: " + PlayerPrefs.GetInt(Tags.HIGHSCORES) + " pts");
+    }
+}
